50aadd76af88b981d24865e349253b1020c13054
Divyansh Bhowmick <35612559+divyanshbhowmick@users.noreply.github.com>
2020-07-13 02:17:47 +0530

    Created the base app!

---
+++ precache-manifest.a7f71171f6cc232c9632874632c74bc6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50c0bdb06c3d9d35c0eb8a2f1afcd05",
+    "url": "/resortmate/index.html"
+  },
+  {
+    "revision": "3444e3fbba51de3546c5",
+    "url": "/resortmate/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "667fdd716dcd2af9fadb",
+    "url": "/resortmate/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3444e3fbba51de3546c5",
+    "url": "/resortmate/static/js/2.336e78d3.chunk.js"
+  },
+  {
+    "revision": "4ecd42502bdf997fb2c4f5b57652fefa",
+    "url": "/resortmate/static/js/2.336e78d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667fdd716dcd2af9fadb",
+    "url": "/resortmate/static/js/main.1cf888a5.chunk.js"
+  },
+  {
+    "revision": "d8afa2c53af26154ce17",
+    "url": "/resortmate/static/js/runtime-main.73f23c02.js"
+  },
+  {
+    "revision": "b3b635eb03ca2c267f72901a17835b90",
+    "url": "/resortmate/static/media/aboutResort.b3b635eb.jpg"
+  },
+  {
+    "revision": "57f4a21d6f4f67ed3d3259c9b7eb2713",
+    "url": "/resortmate/static/media/aboutResort1.57f4a21d.jpg"
+  },
+  {
+    "revision": "58ade6eda0c5561e3ce5b7f5572d55ef",
+    "url": "/resortmate/static/media/aboutResort2.58ade6ed.jpg"
+  },
+  {
+    "revision": "46d6a975e23789d52af57721e8b12aa3",
+    "url": "/resortmate/static/media/background.46d6a975.svg"
+  },
+  {
+    "revision": "33e308d0e4adcb5a09c59f7949679547",
+    "url": "/resortmate/static/media/contact.33e308d0.png"
+  },
+  {
+    "revision": "35307b92622d9fb21959a316ec248a41",
+    "url": "/resortmate/static/media/room.35307b92.glb"
+  }
+]);
